1d6981779e8a4fb7195cb0d20e191129826f83c5
JCGoran <goran.cizmek@gmail.com>
2022-03-24 12:08:17 +0100

    Added install_requires to setup.py script
    Note that some of them may be too new, YMMV.

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@
 
 from distutils.core import setup
 
-setup(name='GaPP',
-      version='1.0',
-      description='Gaussian Processes in Python',
-      author='Marina Seikel',
-      author_email='marina@jorrit.de',
-      url='http://www.acgc.uct.ac.za/~seikel/GAPP/index.html',
-      packages=['gapp', 'gapp.covfunctions'],
-     )
+setup(
+    name='GaPP',
+    version='1.0',
+    description='Gaussian Processes in Python',
+    author='Marina Seikel',
+    author_email='marina@jorrit.de',
+    url='http://www.acgc.uct.ac.za/~seikel/GAPP/index.html',
+    packages=['gapp', 'gapp.covfunctions'],
+    install_requires=[
+        'matplotlib>=3.3',
+        'numpy>=1.16.0',
+        'scipy>=1.2.0',
+        'emcee>=3.1.1',
+        'acor>=1.1.1',
+    ],
+)
